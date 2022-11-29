ec2fa97c784178404c5a196ba0abc33977a7d67d
Ayman Ghorbel <ghorbel.ayman.2000@gmail.com>
2022-11-29 16:29:15 +0100

    new vr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+export default class App extends Component {
+  state = {
+    fullName: "Ayman_Ghorbel",
+    bio: "Welcame to My site web",
+    imgSrc:
+      "https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg",
+    profession: "Full-Stack js Developer",
+    isVisibole: true,
+    counter: 0,
+  };
+  handleshow = () => this.setState({ isVisibole: !this.state.isVisibole });
+  increment = () => this.setState({ counter: this.state.counter + 1 });
+  componentDidMount() {
+    setInterval(() => {
+      this.increment();
+    }, 1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h4>You stay online {this.state.counter} second</h4>
+        <button onClick={this.handleshow}>
+          {this.state.isVisibole ? "hide" : "show"}
+        </button>
+        {this.state.isVisibole && (
+          <div>
+            <h1>My name is {this.state.fullName}</h1>
+            <h2>{this.state.bio}</h2>
+            <img
+              src={this.state.imgSrc}
+              alt=""
+              style={{
+                marginLeft: "-50px",
+                width: "200px",
+                height: "200px",
+                color: "purple",
+                borderRadius: "10px",
+              }}
+            />
+            <h2>I am a {this.state.profession}</h2>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
